e9a279f7f2d8faa770d9d9f3a290c552e87c0e6a
zhangyong <z.hang20080413@163.com>
2020-11-25 18:00:58 +0800

    修改忽略文件

---
+++ src/main/java/cn/net/yzl/ehr/EhrApplication.java
@@ -0,0 +1,13 @@
+package cn.net.yzl.ehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhrApplication.class, args);
+    }
+
+}
