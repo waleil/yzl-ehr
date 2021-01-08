69e2acbefac6a60373d4832b032ddfdda494768a
zhangyongyong <zhangyongyong@yuzhilin.net.cn>
2021-01-08 17:53:28 +0800

    提交测试 配置

--- src/main/java/cn/net/yzl/ehr/vo/SysDictDataVO.java
+++ src/main/java/cn/net/yzl/ehr/vo/SysDictDataVO.java
@@ -6,17 +6,13 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- * 字典数据表
- * 
- * @author：yangxf
- * @date： 2020-12-23 09:58:54
- */
+
 @Data
 @ApiModel(value="SysDictDataVO",description="字典数据实体")
 public class SysDictDataVO {
-//    @ApiModelProperty("字典编码")
-//    private Integer dictCode;
+
+    @ApiModelProperty(value = "字典编码(修改,删除必传)",required = true)
+    private Integer dictCode;
 
 //    @ApiModelProperty("字典排序")
 //    private Integer dictSort;
@@ -24,13 +20,17 @@ public class SysDictDataVO {
 //    @ApiModelProperty("字典标签")
 //    private String dictLabel;
 
-    @ApiModelProperty(value = "字典键值",required = true)
+    @ApiModelProperty(value = "字典键值,添加必传",required = true)
     @NotBlank
     private String dictValue;
 
-    @ApiModelProperty(value = "字典类型,(业务属性:depart_attr,假勤类型:false_name,社保类型:)",required = true)
-    @NotBlank
+    @ApiModelProperty(value = "字典类型,(业务属性:depart_attr,假勤类型:false_name,社保类型:social)",required = true)    @NotBlank
     private String dictType;
+
+
+    @ApiModelProperty(value = "1:新增,2:删除,3:修改",required = true)
+    @NotBlank
+    private Integer flag;
 
 //    @ApiModelProperty("字典类型名称")
 //    private String dictTypeName;
