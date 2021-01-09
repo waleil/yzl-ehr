81e737004d342d9ee718fe47e8d4105df91204a5
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-01-09 14:35:07 +0800

    培训部分参数调整

--- src/main/java/cn/net/yzl/ehr/dto/PostLevelListDto.java
+++ src/main/java/cn/net/yzl/ehr/dto/PostLevelListDto.java
@@ -1,13 +1,15 @@
 package cn.net.yzl.ehr.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class PostLevelListDto {
-
+    @ApiModelProperty("岗位编号")
     private Integer postId;
+    @ApiModelProperty("岗位名称")
     private String postName;
     private List<PostLevelDto> postLevelList;
 }
