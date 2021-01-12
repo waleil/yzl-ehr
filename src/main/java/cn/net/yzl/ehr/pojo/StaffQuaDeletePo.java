572898dd95eeaa9c47902c42c56a8b50481ae511
dingshuting <dingshuting@yzl.net.cn>
2021-01-12 22:27:29 +0800

    完成资质证书接口

---
+++ src/main/java/cn/net/yzl/ehr/pojo/StaffQuaDeletePo.java
@@ -0,0 +1,32 @@
+package cn.net.yzl.ehr.pojo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * staff_up_rp
+ * @author 
+ */
+@Data
+public class StaffQuaDeletePo implements Serializable {
+
+    /**
+     * id 唯一标识
+     */
+    @ApiModelProperty("id 唯一标识")
+    @NotNull
+    @Min(1)
+    private Integer id;
+
+
+    /**
+     * 修改人
+     */
+    @ApiModelProperty(value = "修改人",hidden = true)
+    private String updator;
+}
