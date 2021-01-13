4c1a8ac6378c8284e0aa316ddf936775ddb6c935
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-01-13 12:10:25 +0800

    更改FeignClient

--- src/main/java/cn/net/yzl/ehr/fegin/staff/StaffQuaFeginService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/staff/StaffQuaFeginService.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Repository
-@FeignClient(name = "yzl-staff-db")
+@FeignClient(value = "staff",url = "${fegin.db.url}")
+//@FeignClient(name = "yzl-staff-db")
 public interface StaffQuaFeginService {
 
 
