d6673561553d723cb6c24c2a4a2724198e670a08
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-01-20 16:25:04 +0800

    更改参数

--- src/main/java/cn/net/yzl/ehr/controller/FastDFSController.java
+++ src/main/java/cn/net/yzl/ehr/controller/FastDFSController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/file")
 @Api(value = "文件上传", tags = {"文件上传"})
@@ -17,13 +19,8 @@ public class FastDFSController {
     private FastDFSClientWrapper client;
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String upload(MultipartFile file) {
-        String path=null;
-        try {
-             path = client.uploadFile(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String upload(MultipartFile file) throws IOException {
+        String path = client.uploadFile(file);
         return path;
     }
 }
