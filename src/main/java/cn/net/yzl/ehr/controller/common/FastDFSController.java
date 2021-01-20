8dbfdd3701d98ba1c6d65310087dc8e277d22dd7
zhangyongyong <zhangyongyong@yuzhilin.net.cn>
2021-01-20 16:31:03 +0800

    修改公共接口 员工基本信息接口

---
+++ src/main/java/cn/net/yzl/ehr/controller/common/FastDFSController.java
@@ -0,0 +1,28 @@
+package cn.net.yzl.ehr.controller.common;
+
+import cn.net.yzl.ehr.util.FastDFSClientWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/file")
+@Api(value = "公共接口", tags = {"公共接口"})
+public class FastDFSController {
+
+    @Autowired
+    private FastDFSClientWrapper client;
+    @ApiOperation(value = "文件上传", notes = "文件上传")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(MultipartFile file) throws IOException {
+        String path = client.uploadFile(file);
+        return path;
+    }
+}
