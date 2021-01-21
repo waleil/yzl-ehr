df1887fb4195ab8c94f6cee4df51921bfe4f9242
w-yinjunkai <w-yinjunkai@yuzhilin.net.cn>
2021-01-21 09:23:32 +0800

    提交代码

--- src/main/java/cn/net/yzl/ehr/controller/StaffEntryPostController.java
+++ src/main/java/cn/net/yzl/ehr/controller/StaffEntryPostController.java
@@ -1,24 +1,41 @@
-//package cn.net.yzl.ehr.controller;
-//
-//import cn.net.yzl.common.entity.ComResponse;
-//
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/staff/entrypost")
-//@Api(value = "员工入岗", tags = {"员工档案"})
-//public class StaffEntryPostController {
-////    @Autowired
-////    private StaffEntryPostService staffEntryPostService;
-//
-//    @ApiOperation(value = "员工入岗-员工入岗", notes = "员工入岗", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-//    @RequestMapping(value = "/insert",method = RequestMethod.POST)
-//    ComResponse<Integer> insert(@RequestParam("staffNo") String staffNo) {
-//        return staffEntryPostService.insert(staffNo);
-//    }
-//
-//}
+package cn.net.yzl.ehr.controller;
+
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.ehr.authorization.annotation.CurrentStaffNo;
+import cn.net.yzl.ehr.dto.StaffEduListDto;
+import cn.net.yzl.ehr.pojo.*;
+import cn.net.yzl.ehr.service.StaffEduService;
+import cn.net.yzl.ehr.service.StaffEntryPostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff/entrypost")
+@Api(value = "员工入岗", tags = {"员工档案"})
+public class StaffEntryPostController {
+    @Autowired
+    private StaffEntryPostService staffEntryPostService;
+
+    @ApiOperation(value = "员工入岗—入岗",notes = "员工入岗",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "StaffNo", value = "员工工号", required = true, paramType = "query")
+    )
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    ComResponse<Integer> findByStaffNo(@RequestParam("StaffNo") String staffNo) {
+        return staffEntryPostService.insert(staffNo);
+    }
+
+
+}
--- src/main/java/cn/net/yzl/ehr/fegin/staff/StaffEntryPostFeginService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/staff/StaffEntryPostFeginService.java
@@ -1,27 +1,27 @@
 package cn.net.yzl.ehr.fegin.staff;
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.ehr.dto.StaffWorkListDto;
-import cn.net.yzl.ehr.pojo.StaffWorkInsertListPo;
-import cn.net.yzl.ehr.pojo.StaffWorkItemPo;
-import cn.net.yzl.ehr.pojo.StaffWorkUpdateListPo;
-import cn.net.yzl.ehr.pojo.StaffWorkUpdatePo;
+import cn.net.yzl.ehr.dto.StaffEduListDto;
+import cn.net.yzl.ehr.pojo.*;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 
-//@Repository
+@Repository
+@FeignClient(value = "staff",url = "${fegin.db.url}")
 //@FeignClient(name = "yzl-staff-db")
-//public interface StaffEntryPostFeginService {
-//
-//    @ApiOperation(value = "员工入岗", notes = "员工入岗")
-//    @RequestMapping(value = "/entrypost/insert", method = RequestMethod.POST,consumes = "application/json")
-//    ComResponse<Integer> insert(@RequestParam("staffNo") String staffNo);
-//
-//
-//}
+public interface StaffEntryPostFeginService {
+
+
+    @ApiOperation(value = "员工入岗",notes = "员工入岗",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @RequestMapping(value = "/entrypost/insert", method = RequestMethod.POST)
+    ComResponse<Integer> insert(@RequestParam("staffNO")  String staffNo);
+
+}
