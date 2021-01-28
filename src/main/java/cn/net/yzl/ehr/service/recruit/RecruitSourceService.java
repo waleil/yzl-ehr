3b1bb711ec8e54beca8479d263fbd476c38105c0
dingshuting <dingshuting@yzl.net.cn>
2021-01-28 21:08:05 +0800

    修改招聘渠道接口

--- src/main/java/cn/net/yzl/ehr/service/recruit/RecruitSourceService.java
+++ src/main/java/cn/net/yzl/ehr/service/recruit/RecruitSourceService.java
@@ -1,6 +1,7 @@
 package cn.net.yzl.ehr.service.recruit;
 
 import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.staff.dto.recruit.RecruitSourceDto;
 import cn.net.yzl.staff.pojo.recruit.RecruitSourceInsertPo;
 import cn.net.yzl.staff.pojo.recruit.RecruitSourceUpdateListPo;
@@ -22,4 +23,6 @@ public interface RecruitSourceService {
     ComResponse<Integer> update(RecruitSourceUpdatePo updatePo);
 
     ComResponse<Integer> getRecruitInfo(@RequestBody RecruitSourceUpdatePo updatePo);
+
+    ComResponse<Page<RecruitSourceDto>> queryPage(Integer pageSize, Integer pageNum);
 }
