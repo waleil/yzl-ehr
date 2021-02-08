eb0ba446897e3b05f453dce16b9f3f21ec210bc1
dingshuting <dingshuting@yzl.net.cn>
2021-02-08 21:02:57 +0800

    修改办公物品管理接口,和招聘渠道接口

--- src/main/java/cn/net/yzl/ehr/fegin/resource/CourseWareFeginService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/resource/CourseWareFeginService.java
@@ -4,6 +4,7 @@ import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.ehr.dto.CourseWareCategoryDto;
 
+
 import cn.net.yzl.staff.pojo.courseWare.CourseSelectPo;
 import cn.net.yzl.staff.pojo.courseWare.CourseWareCategoryPo;
 import cn.net.yzl.staff.pojo.courseWare.CourseWarePo;
@@ -20,8 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Repository
-@FeignClient(name = "yzl-staff-db")
-//@FeignClient(value = "staff",url = "${fegin.db.url}")
+//@FeignClient(name = "yzl-staff-db")
+@FeignClient(value = "staff",url = "${fegin.db.url}")
 public interface CourseWareFeginService {
 
     @ApiOperation(value = "新增课件", notes = "新增课件", consumes = MediaType.APPLICATION_JSON_VALUE)
