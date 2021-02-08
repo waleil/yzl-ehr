6c641434d7c411ff25d82396acc8b8d1def0d160
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-02-08 14:02:58 +0800

    注解错误修改

--- src/main/java/cn/net/yzl/ehr/fegin/salaryRule/SalaryFrontLineRuleFeginService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/salaryRule/SalaryFrontLineRuleFeginService.java
@@ -6,10 +6,7 @@ import cn.net.yzl.staff.dto.salaryRule.SalaryRuleDepartPostDto;
 import cn.net.yzl.staff.vo.salaryRule.*;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,7 +43,7 @@ public interface SalaryFrontLineRuleFeginService {
     ComResponse<Boolean> rule9(@RequestBody SalaryFrontLineRuleEditVo9 salaryFrontLineRuleEditVo9);
 
     @RequestMapping(value = "/getListByDepartId", method = RequestMethod.GET)
-    ComResponse<List<SalaryRuleDepartPostDto>> getListByDepartId(@Param("departId") Integer departId, @Param("ruleType") Integer ruleType);
+    ComResponse<List<SalaryRuleDepartPostDto>> getListByDepartId(@RequestParam("departId") Integer departId, @RequestParam("ruleType") Integer ruleType);
 
     @PostMapping("/ruleDepartPostConfig")
     ComResponse<Boolean> ruleDepartPostConfig(@RequestBody SalaryRulePostVo salaryRulePostVo);
