afa97e2c1f4dfcdc93d4277d8229b32f07d08e7d
wangyan <wangyanasj@sina.com>
2021-02-19 16:25:54 +0800

    我的工资

--- src/main/java/cn/net/yzl/ehr/fegin/salary/SalaryService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/salary/SalaryService.java
@@ -2,7 +2,9 @@ package cn.net.yzl.ehr.fegin.salary;
 
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.staff.dto.salary.MySalaryDto;
 import cn.net.yzl.staff.dto.salary.SalaryDto;
+import cn.net.yzl.staff.vo.salary.MySalaryVo;
 import cn.net.yzl.staff.vo.salary.SalaryFinanceVo;
 import cn.net.yzl.staff.vo.salary.SalaryVo;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -32,4 +34,7 @@ public interface SalaryService {
     @PostMapping("/postToFinance")
     ComResponse<Void>  postToFinance(@RequestBody List<SalaryFinanceVo> list);
 
+    //我得工资
+    @RequestMapping(value = "/getMySalary", method = RequestMethod.GET)
+    ComResponse<MySalaryDto> getMySalary(MySalaryVo request);
 }
