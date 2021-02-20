0ccbdb5cef02539e2d034a91b6f80508981dc71c
hzg <3157888633@qq.com>
2021-02-20 17:27:12 +0800

    流程审批：审批流程配置优化

--- src/main/java/cn/net/yzl/ehr/fegin/process/ProcessConfigFeignService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/process/ProcessConfigFeignService.java
@@ -2,6 +2,7 @@ package cn.net.yzl.ehr.fegin.process;
 
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.staff.dto.StaffLevelDto;
 import cn.net.yzl.staff.dto.process.ProcessConfigDetailDto;
 import cn.net.yzl.staff.dto.process.ProcessDto;
 import cn.net.yzl.staff.vo.process.ProcessConfigPageVo;
@@ -12,24 +13,29 @@ import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @Repository
-@FeignClient(value = "staff",url = "${fegin.db.url}/process")
+@FeignClient(value = "staff",url = "${fegin.db.url}")
 @RefreshScope
 public interface ProcessConfigFeignService {
 
-    @RequestMapping(value = "/config/insert", method = RequestMethod.POST)
+    @RequestMapping(value = "/process/config/insert", method = RequestMethod.POST)
     ComResponse<Integer> insertProcessConfig (@RequestBody @Validated ProcessConfigVo processConfigVo, @RequestParam("staffNo") String staffNo);
 
-    @RequestMapping(value = "/config/delete", method = RequestMethod.POST)
+    @RequestMapping(value = "/process/config/delete", method = RequestMethod.POST)
     ComResponse<Integer> deleteProcessConfig (@RequestParam("id") Integer id,@RequestParam("staffNo") String staffNo);
 
-    @RequestMapping(value = "/config/disable", method = RequestMethod.POST)
+    @RequestMapping(value = "/process/config/disable", method = RequestMethod.POST)
     ComResponse<Integer> disableProcessConfig (@RequestParam("id") Integer id,@RequestParam("staffNo") String staffNo);
 
-    @RequestMapping(value = "/config/pageSelect", method = RequestMethod.POST)
+    @RequestMapping(value = "/process/config/pageSelect", method = RequestMethod.POST)
     ComResponse<Page<ProcessDto>> pageSelectProcessConfig(@RequestBody @Validated ProcessConfigPageVo processConfigPageVo) ;
 
-    @RequestMapping(value = "/config/detail", method = RequestMethod.GET)
+    @RequestMapping(value = "/process/config/detail", method = RequestMethod.GET)
     ComResponse<ProcessConfigDetailDto> processConfigDetail (@RequestParam("id") Integer id);
+
+    @RequestMapping(value = "/staff/getStaffLevelByStaffNo", method = RequestMethod.GET)
+    ComResponse<List<StaffLevelDto>> getStaffLevelByStaffNo(@RequestParam("staffNo") String staffNo, @RequestParam("flag") Integer flag) ;
 }
