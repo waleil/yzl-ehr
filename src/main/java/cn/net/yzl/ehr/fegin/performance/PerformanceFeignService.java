e20738f7f98d956c98d107c0d3ad647acc72f362
别宝杰 <biebaojie@yuzhilin.net.cn>
2021-02-24 14:56:34 +0800

    替换绩效feign路径

--- src/main/java/cn/net/yzl/ehr/fegin/performance/PerformanceFeignService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/performance/PerformanceFeignService.java
@@ -25,8 +25,8 @@ import java.util.List;
  *
  * @author biebaojie
  */
-//@FeignClient(value = "PerformanceFeignService", url = "${fegin.db.url}/performance")
-@FeignClient(value = "PerformanceFeignService", url = "http://localhost:38080/performance")
+@FeignClient(value = "PerformanceFeignService", url = "${fegin.db.url}/performance")
+//@FeignClient(value = "PerformanceFeignService", url = "http://localhost:38080/performance")
 @RefreshScope
 public interface PerformanceFeignService {
 
