2aeb2bc86e24dd41fd14f55cdc2facc85bcb6eb1
w-cuileilei <810435611@qq.com>
2021-02-26 11:57:27 +0800

    转正、离职代码

--- src/main/java/cn/net/yzl/ehr/controller/process/ProcessBeginController.java
+++ src/main/java/cn/net/yzl/ehr/controller/process/ProcessBeginController.java
@@ -3,7 +3,9 @@ package cn.net.yzl.ehr.controller.process;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.enums.ResponseCodeEnums;
 import cn.net.yzl.ehr.fegin.processActiveService.saveProcessService;
+import cn.net.yzl.staff.dto.personApprove.ApproveDimissionInfoListDTO;
 import cn.net.yzl.staff.dto.personApprove.ApproveInviteDTO;
+import cn.net.yzl.staff.dto.personApprove.ApprovePostInfoListDTO;
 import cn.net.yzl.staff.exception.BaseParamsException;
 import cn.net.yzl.staff.vo.process.ProcessStaffDimissionVo;
 import cn.net.yzl.staff.vo.process.ProcessStaffPositiveVo;
@@ -32,15 +34,14 @@ public class ProcessBeginController {
     }
     @ApiOperation(value = "保存转正申请",notes = "转正申请添加",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @RequestMapping(value = "v1/savePositiveApplay", method = RequestMethod.POST)
-    ComResponse<Boolean> savePositiveApplay (@RequestBody @Validated ProcessStaffPositiveVo staffPositiveVo){
+    ComResponse<Boolean> savePositiveApplay (@RequestBody @Validated ApprovePostInfoListDTO approvePostInfoListDTO){
 
-
-        return saveProcessService.savePositiveApplay(staffPositiveVo);
+        return saveProcessService.savePositiveApplay(approvePostInfoListDTO);
     }
     @ApiOperation(value = "保存离职申请",notes = "离职申请添加",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @RequestMapping(value = "v1/saveDimissionApplay", method = RequestMethod.POST)
-    ComResponse<Boolean> saveDimissionApplay (@RequestBody @Validated ProcessStaffDimissionVo staffDimissionVo){
+    ComResponse<Boolean> saveDimissionApplay (@RequestBody @Validated ApproveDimissionInfoListDTO approveDimissionInfoListDTO){
 
-        return saveProcessService.saveDimissionApplay(staffDimissionVo);
+        return saveProcessService.saveDimissionApplay(approveDimissionInfoListDTO);
     }
 }
