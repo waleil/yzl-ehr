081975a0ddda630380af5c2270e7fe04d243bdf9
zhangyongyong <zhangyongyong@yuzhilin.net.cn>
2021-03-05 11:34:17 +0800

    修改参数

--- src/main/java/cn/net/yzl/ehr/controller/resume/ResumeInterviewOfferController.java
+++ src/main/java/cn/net/yzl/ehr/controller/resume/ResumeInterviewOfferController.java
@@ -1,11 +1,14 @@
 package cn.net.yzl.ehr.controller.resume;
 
+import cn.hutool.json.JSONUtil;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.ehr.authorization.annotation.CurrentStaffNo;
 import cn.net.yzl.ehr.fegin.resume.ResumeInterviewOfferFeginService;
 import cn.net.yzl.staff.vo.resume.ResumeInterviewOfferInsertVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
@@ -20,13 +23,14 @@ import springfox.documentation.annotations.ApiIgnore;
 @Api(value = "人事管理-面试管理", tags = {"人事管理"})
 @Validated
 public class ResumeInterviewOfferController {
-
+    private static final Logger logger = LoggerFactory.getLogger(ResumeInterviewOfferController.class);
     @Autowired
     private ResumeInterviewOfferFeginService resumeInterviewOfferFeginService;
 
     @ApiOperation(value = "简历列表-发送offer", notes = "简历列表-发送offer", consumes = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping(value = "/sendOffer", method = RequestMethod.POST)
     ComResponse<String> sendOffer(@RequestBody  ResumeInterviewOfferInsertVO resumeInterviewOfferInsertVO,@ApiIgnore @CurrentStaffNo String staffNo) throws IllegalAccessException {
+        logger.info("/resume/offer/sendOffer :  params {}", JSONUtil.toJsonStr(resumeInterviewOfferInsertVO));
         return resumeInterviewOfferFeginService.sendOffer(resumeInterviewOfferInsertVO);
     }
 
