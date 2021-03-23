183963e23ad515657f8d4cc6aebca5cde5b2de20
dingshuting <dingshuting@yzl.net.cn>
2021-03-23 20:02:16 +0800

    解决BUG号EHR-1218 奖惩管理--查看扣款详情,目前数据显示不全.加上分页,

--- src/main/java/cn/net/yzl/ehr/fegin/deduct/DeductItemFeginService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/deduct/DeductItemFeginService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -37,7 +38,7 @@ public interface DeductItemFeginService {
 
     @ApiOperation(value = "查询所有",notes = "查询",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @RequestMapping(value = "/deduct/queryItem", method = RequestMethod.GET)
-    ComResponse<Page<DeductItemDto>> queryItem(Integer pageNum,Integer pageSize);
+    ComResponse<Page<DeductItemDto>> queryItem(@RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize")Integer pageSize);
 
     @ApiOperation(value ="编辑扣款类型" ,notes ="编辑扣款类型",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @RequestMapping(value = "/deduct/update",method = RequestMethod.POST)
