f7467b717e020f178a945c90d2f0ca01f701b5b7
jinhongxing <jinhongxing@yuzhilin.net.cn>
2021-04-14 10:58:18 +0800

    1.修改调用路径

--- src/main/java/cn/net/yzl/ehr/fegin/ultrcrm/GroupFeignService.java
+++ src/main/java/cn/net/yzl/ehr/fegin/ultrcrm/GroupFeignService.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "yzl-cti")
+@FeignClient(name = "yzl-cti",url = "${fegin.cti.url}/cti")
 @Repository
 public interface GroupFeignService {
 
-    @RequestMapping(value = "/cti/buss/queryGroups", method = RequestMethod.GET)
+    @RequestMapping(value = "/buss/queryGroups", method = RequestMethod.GET)
     ComResponse<List<Group>> queryGroups( @RequestParam("groupType")Integer groupType, @RequestParam("fatherId")String fatherId);
 
-    @RequestMapping(value = "/cti/buss/addGroup", method = RequestMethod.POST)
+    @RequestMapping(value = "/buss/addGroup", method = RequestMethod.POST)
     ComResponse<Boolean> addGroup( @RequestBody GroupVo group);
 
-    @RequestMapping(value = "/cti/buss/delGroup", method = RequestMethod.GET)
+    @RequestMapping(value = "/buss/delGroup", method = RequestMethod.GET)
     ComResponse<Boolean> delGroup(@RequestParam("groupIds")String groupIds,@RequestParam("groupType")Integer groupType);
 
-    @RequestMapping(value = "/cti/buss/updateGroup", method = RequestMethod.GET)
+    @RequestMapping(value = "/buss/updateGroup", method = RequestMethod.GET)
     ComResponse<Boolean> updateUltrGroup(@RequestParam(value = "groupId") String groupId,@RequestParam(value = "groupType") Integer groupType,@RequestParam(value = "groupName") String groupName);
 }
