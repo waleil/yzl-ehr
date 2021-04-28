29c96c4947bd37a29e72bb0af98deed3a0cc3c98
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-04-28 18:56:03 +0800

    二次入职专用 -用身份证查询员工信息时,返回是否黑名单

--- src/main/java/cn/net/yzl/ehr/dto/StaffBaseDto.java
+++ src/main/java/cn/net/yzl/ehr/dto/StaffBaseDto.java
@@ -1,11 +1,13 @@
 package cn.net.yzl.ehr.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -24,5 +26,19 @@ public class StaffBaseDto implements Serializable {
     @ApiModelProperty(value="密码",name="password",hidden = true)
     private String password;
 
-
+    @ApiModelProperty(value="部门岗位编号",name="departPostId")
+    private String departPostId;
+    @ApiModelProperty(value="部门编号",name="departId")
+    private String departId;
+    @ApiModelProperty(value="岗位编号",name="postId")
+    private String postId;
+    @ApiModelProperty(value="部门名称",name="departName")
+    private String departName;
+    @ApiModelProperty(value="岗位名称",name="postName")
+    private String postName;
+    @ApiModelProperty(value="入职时间",name="entryTime")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
+    private Date entryTime;
+    @ApiModelProperty(value="是否在黑名单中:0否 1是",name="onBlackList")
+    private Integer onBlackList;
 }
